227207abfead09d565eaa8f0ca0c8daafbcfd001
Tim Herd <rz@eqdw.net>
2013-01-08 19:54:17 -0800

    KILLED BRAD ._.

--- backend/brad/test.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-require_once("Pusher.php");
-
-$pusher = new Pusher("c5ba61579edbf533d0fe", "07fd14a2b945d86fb21b", "xxx", true, "54.235.244.144", "4567");
-
-$pusher->trigger('my-channel', 'my_event', 'hello world');
-
-?>

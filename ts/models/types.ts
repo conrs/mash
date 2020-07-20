f9b4f81ba50d329c5b60d7da13a72fec697cff05
Matt Connors <matt@con.rs>
2020-07-19 23:06:31 -0700

    v0.1.6

--- ts/models/types.ts
+++
@@ -1,9 +0,0 @@
-import { Stream }from "ts-stream"
-
-export type ProgramInput = {
-    stdin: Stream<string>,
-    stdout: Stream<string>,
-    args?: string[]
-}
-
-export type Program = (input: ProgramInput) => Promise<number>

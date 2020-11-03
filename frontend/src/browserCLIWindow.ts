92f9ecc8d5171bf55f126f3a147257a59ee1f38b
Matt Connors <matt@con.rs>
2020-11-03 00:27:03 -0800

    scrollTop woes

--- frontend/src/browserCLIWindow.ts
+++ frontend/src/browserCLIWindow.ts
@@ -71,7 +71,7 @@ export class BrowserCLIWindow {
       if(this.yShouldBeAtBottom) {
         this.yShouldBeAtBottom = false
         this.cursorY = this.outputElement.innerText.split("\n").length - 1
-        document.body.scrollTop = Number.MAX_SAFE_INTEGER
+        document.body.scrollTop = document.body.scrollHeight
       }
 
       this.cursorElement.style.left = (this.cursorX * ((Math.round(cursorElement.getBoundingClientRect().width * 10000)*1.11) / 10000)).toString()
@@ -109,8 +109,6 @@ export class BrowserCLIWindow {
           if(mash.util.Ascii.isVisibleText(character)) {
             this.buffer += mash.util.Ascii.characterCodesToString([character])
           }
-          
-          document.body.scrollTop = Number.MAX_SAFE_INTEGER
 
           break;
       }
